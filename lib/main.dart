3efc4ab37b28e206f7b8b9d41ce9e27ca029d709
Thiago Veloso <thiago.vbs@gmail.com>
2026-06-01 12:45:06 -0300

    fix: correção nome da aplicação

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'SRO Ponto',
+      title: 'Ponto Eletrônico',
       debugShowCheckedModeBanner: false,
       locale: const Locale('pt', 'BR'),
       theme: ThemeData(primarySwatch: Colors.blue, useMaterial3: true),
